15786fc90ce2fadb7766eb05721563ad4770989a
Proma Chow <bsse1132@iit.du.ac.bd>
2022-03-22 02:22:06 +0600

    can read data from firestore

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth} from 'firebase/auth';
-import { getDatabase } from "firebase/database";
+import { getFirestore, doc, getDoc } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"; 
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app);
-const user = {email:'jhabdjhabd.gmail.com'};
-const res = await db.collection('Users').doc('user_1').set(user);
-export {app,auth,db}
+const db = getFirestore(app);
+
+export{app,auth,db}
--- screens/Main.js
+++ screens/Main.js
@@ -1,19 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView,StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { SafeAreaView,StyleSheet, Text, View,TouchableOpacity,Button } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { app,db } from '../firebase-config';
+import { collection, getDocs } from "firebase/firestore"; 
 import {styles} from "../styles";
 
 
 //const res = await db.collection('Users').doc('user_1').set(user);
 export const Main = () => {
+  
+    const getData = async()=>{
+     
+    const querySnapshot = await getDocs(collection(db, "Users"));
+    querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+});
+    }
+
     const user = {Name: 'abcd', Age : 20};
     return(
 
       <SafeAreaView style={styles.container}>
-        <Text>{user.Name}, {user.Age}</Text>
+        <Text>{user.Name}</Text>
+        <Button title="Proceed" onPress={getData} style ={{backgroundColor:"#0076653"}}></Button>
       </SafeAreaView>
     );
   }

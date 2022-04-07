049ad9d192b1e9c9ec05e15c3b3908af1a8c29c3
Proma Chow <bsse1132@iit.du.ac.bd>
2022-04-08 00:54:17 +0600

    added tesseract without image processing

--- flask/App.py
+++ flask/App.py
@@ -4,6 +4,8 @@ from flask_cors import CORS, cross_origin
 import sys
 from flask import jsonify
 import base64
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -17,6 +19,7 @@ def hello_world():
         print(type(bytesOfimage['image']), file=sys.stderr)
         with open("imageToSave.png", "wb") as fh:
             fh.write(base64.b64decode(bytesOfimage['image']))
+        print(pytesseract.image_to_string(Image.open('imageToSave.png')))
         # with open('image.jpeg', 'wb') as out:
         #     out.write(bytesOfimage)
     return "<p>Hello, World!</p>"

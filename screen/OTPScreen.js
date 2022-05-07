45bc761d472ef972eaa894ff1afb1ff99cfd5f09
Proma Chow <bsse1132@iit.du.ac.bd>
2022-05-07 14:03:42 +0600

    sign up UI added

--- screen/OTPScreen.js
+++ screen/OTPScreen.js
@@ -1,58 +1,235 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
-import { confirmCode } from '../Authentication';
+import {confirmCode} from '../Authentication';
 import firestore from '@react-native-firebase/firestore';
-import { add_User } from '../FireStoreHelperFunctions';
-import firebase from '@react-native-firebase/app'
+import {add_User} from '../FireStoreHelperFunctions';
+import firebase from '@react-native-firebase/app';
+import OTPInputView from '@twotalltotems/react-native-otp-input';
+import AnimatedLinearGradient, {
+  presetColors,
+} from 'react-native-animated-linear-gradient';
+import CountDown from 'react-native-countdown-component';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/Feather';
+import MaskedView from '@react-native-community/masked-view';
+import LinearGradient from 'react-native-linear-gradient';
+
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    Button,
-    TextInput
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
-export const OTP = ({ navigation }) => {
+export const OTP = ({route, navigation}) => {
+  const [code, setCode] = React.useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
+  const {phoneNumber} = route.params;
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
-    const [code, setCode] = React.useState('');
+  console.log('code from OTP' + code);
+  return (
+    <SafeAreaView style={styles.content}>
+      <Modal
+        transparent={true}
+        isVisible={isModalVisible}
+        onBackdropPress={() => setModalVisible(false)}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              width: 300,
+              height: 300,
+              backgroundColor: '#033030',
+              opacity: 0.5,
+              justifyContent: 'center',
+              borderRadius: 15,
+            }}>
+            <Text
+              style={{
+                color: '#FFFFFF',
+                alignSelf: 'center',
+                marginTop: 10,
+                fontFamily: 'fantasy',
+                fontSize: 20,
+              }}>
+              Sorry
+            </Text>
+            <Text
+              style={{
+                color: '#FFFFFF',
+                alignSelf: 'center',
+                margin: 20,
+                marginTop: 40,
+                fontFamily: 'fantasy',
+              }}>
+              If you didn't get the code by SMS, please check your cellular data
+              settings and phone number
+            </Text>
+            <TouchableOpacity
+              style={styles.modalButtonContainer}
+              onPress={async () => {
+                navigation.navigate('SignUp');
+              }}>
+              <Text
+                style={{
+                  color: '#FFFFFF',
+                  fontSize: 12,
+                  fontFamily: 'fantasy',
+                  alignSelf: 'center',
+                }}>
+                EDIT PHONE NUMBER
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
-    console.log("code from OTP" + code);
-    return (
-        <SafeAreaView>
-            <TextInput style={{ backgroundColor: "#004567" }} keyboardType="phone-pad"
-                onChangeText={setCode} placeholder="Enter OTP"
-            ></TextInput>
-            <Button title="Proceed" style={{ backgroundColor: "#000000" }}
-                onPress={async() => {
-                    console.log("Otp")
-                    await confirmCode(code).then(()=>{
-                    const user = firebase.auth().currentUser;
-                    console.log("o"+user.uid);
-                    if(!user) console.log("hi");
-                    firestore()
-                    .collection('Users')
-                    .doc(user.uid)
-                    .get()
-                    .then(documentSnapshot => {
-                        let exists = documentSnapshot.exists;
-                        console.log(user.uid +" "+ exists);
-                        if (!exists) {
-                             
-                             add_User(user.uid);
-                             navigation.navigate('Profile');
-                        }
-                        else navigation.navigate('Feed');
-                    });
-                }
-                    )
-            }
-            }
-          ></Button>
+      <View style={{alignContent: 'center'}}>
+        <Icon
+          name="key"
+          color="#115e59"
+          size={130}
+          style={{opacity: 0.7, marginTop: 100}}></Icon>
+      </View>
+      <View>
+        <OTPInputView
+          style={{width: '70%', height: 200}}
+          pinCount={6}
+          // code={this.state.code} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
+          // onCodeChanged = {code => { this.setState({code})}}
+          autoFocusOnLoad
+          codeInputFieldStyle={{
+            backgroundColor: '#115e59',
+            opacity: 0.3,
+          }}
+          codeInputHighlightStyle={{
+            backgroundColor: '#14532d',
+          }}
+          onCodeFilled={code => {
+            setCode(code);
+            console.log(`Code is ${code}, you are good to go!`);
+          }}></OTPInputView>
+      </View>
 
-        </SafeAreaView >
-    )
-}
+      <View>
+        <TouchableOpacity
+          style={styles.appButtonContainer}
+          onPress={async () => {
+            console.log('Otp');
+            await confirmCode(code).then(() => {
+              const user = firebase.auth().currentUser;
+              console.log('o' + user.uid);
+              if (!user) console.log('hi');
+              firestore()
+                .collection('Users')
+                .doc(user.uid)
+                .get()
+                .then(documentSnapshot => {
+                  let exists = documentSnapshot.exists;
+                  console.log(user.uid + ' ' + exists);
+                  if (!exists) {
+                    add_User(user.uid);
+                    navigation.navigate('Profile');
+                  } else navigation.navigate('Feed');
+                });
+            });
+          }}>
+          <Text style={styles.appButtonText}>Proceed</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity onPress={toggleModal}>
+          <Text style={{color: '#0d9488', fontSize: 15, fontFamily: 'fantasy'}}>
+            Didn't receive code?
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 3,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  contentButton: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  borderStyleBase: {
+    width: 80,
+    height: 45,
+    borderColor: '#0d9488',
+    backgroundColor: '#3d4f4d',
+  },
+
+  borderStyleHighLighted: {
+    borderColor: '#115e59',
+    borderWidth: 10,
+  },
+
+  underlineStyleBase: {
+    width: 30,
+    height: 45,
+    borderWidth: 0,
+    borderBottomWidth: 10,
+    color: '#115e59',
+  },
+
+  underlineStyleHighLighted: {
+    borderColor: '#115e59',
+  },
+
+  appButtonContainer: {
+    elevation: 15,
+    backgroundColor: '#86b8b2',
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderColor: '#115e59',
+  },
+
+  modalButtonContainer: {
+    elevation: 15,
+    backgroundColor: '#86b8b2',
+    borderRadius: 10,
+    paddingVertical: 20,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor: '#115e59',
+    margin: 40,
+  },
+
+  appButtonText: {
+    fontSize: 14,
+    color: '#fff',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+});
--- screen/SignUp.js
+++ screen/SignUp.js
@@ -1,41 +1,177 @@
-
-import React,{useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import auth from '@react-native-firebase/auth';
-import firebase from '@react-native-firebase/app'
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { signInWithPhoneNumber } from '../Authentication';
+import firebase from '@react-native-firebase/app';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {signInWithPhoneNumber} from '../Authentication';
+import PhoneInput from 'react-native-phone-number-input';
+import Icon from 'react-native-vector-icons/Feather';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    Button,
-    TextInput
-  } from 'react-native';
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export const SignUp = () => {
   const navigation = useNavigation();
-  const [phoneNumber,setPhoneNumber] = React.useState();
+  const [phoneNumber, setPhoneNumber] = React.useState();
+  const [formattedValue, setFormattedValue] = useState('');
+  const [valid, setValid] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+
   const [user, setUser] = React.useState();
   const [initializing, setInitializing] = useState(true);
- 
+  console.log(formattedValue);
+  return (
+    <SafeAreaView style={styles.content}>
+      <View style={{height: 200}}>
+        <Icon
+          name="key"
+          color="#115e59"
+          size={180}
+          style={{opacity: 0.7, alignSelf: 'center'}}></Icon>
+      </View>
+      <View style={{height: 100}}>
+        <Text
+          style={{
+            color: '#115e59',
+            alignSelf: 'center',
+            margin: 20,
+            marginTop: 40,
+            fontFamily: 'fantasy',
+            opacity: 0.8,
+          }}>
+          Please select the country code and enter your phone number
+        </Text>
+      </View>
 
-  console.log("phone"+phoneNumber);
-  
-  return(
-      <SafeAreaView>
-          <TextInput style={{backgroundColor: "#004567"}} keyboardType="phone-pad"
-                     onChangeText={setPhoneNumber} placeholder="Enter your Phone Number"
-          ></TextInput>
-          <Button title ="Proceed" style={{backgroundColor: "#000000"}} 
-                  onPress = {async()=>{ await signInWithPhoneNumber(phoneNumber).then(()=> navigation.navigate('OTP'))
-                                 }}
-          ></Button>
-      </SafeAreaView>
-  )
-}
+      <View style={{height: 100}}>
+        <PhoneInput
+          containerStyle={{
+            backgroundColor: '#115e59',
+            opacity: 0.7,
+            borderRadius: 4,
+            width: 370,
+            height: 50,
+            paddingVertical: 0,
+          }}
+          textInputStyle={{
+            fontSize: 15,
+            height: 50,
+            paddingVertical: 0,
+            color: '#FFFFFF',
+          }}
+          codeTextStyle={{
+            fontSize: 14,
+            height: 50,
+            paddingVertical: 0,
+            marginTop: 29,
+            color: '#FFFFFF',
+          }}
+          defaultValue={phoneNumber}
+          defaultCode="DM"
+          layout="first"
+          onChangeText={text => {
+            setPhoneNumber(text);
+          }}
+          onChangeFormattedText={text => {
+            setFormattedValue(text);
+          }}
+          autoFocus
+        />
+      </View>
+      <View style={{height: 100}}>
+        <TouchableOpacity
+          style={styles.appButtonContainer}
+          onPress={async () => {
+            await signInWithPhoneNumber(formattedValue).then(() => {
+              navigation.navigate('OTP', {
+                phone: phoneNumber,
+              });
+            });
+          }}>
+          <Text style={styles.appButtonText}>Proceed</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 3,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  contentButton: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  borderStyleBase: {
+    width: 80,
+    height: 45,
+    borderColor: '#0d9488',
+    backgroundColor: '#3d4f4d',
+  },
+
+  borderStyleHighLighted: {
+    borderColor: '#115e59',
+    borderWidth: 10,
+  },
+
+  underlineStyleBase: {
+    width: 30,
+    height: 45,
+    borderWidth: 0,
+    borderBottomWidth: 10,
+    color: '#115e59',
+  },
+
+  underlineStyleHighLighted: {
+    borderColor: '#115e59',
+  },
+
+  appButtonContainer: {
+    elevation: 15,
+    backgroundColor: '#86b8b2',
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderColor: '#115e59',
+  },
+
+  modalButtonContainer: {
+    elevation: 15,
+    backgroundColor: '#86b8b2',
+    borderRadius: 10,
+    paddingVertical: 20,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor: '#115e59',
+    margin: 40,
+  },
+
+  appButtonText: {
+    fontSize: 14,
+    color: '#fff',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+});

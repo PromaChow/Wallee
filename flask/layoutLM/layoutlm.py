e9ec5632cf74161766ff0982e3933e08febf9227
PromaChow <bsse1132@iit.du.ac.bd>
2022-05-31 01:43:35 +0600

    fixed layoutlm

---
+++ flask/layoutLM/layoutlm.py
@@ -0,0 +1,263 @@
+import numpy as np
+import pytesseract
+from PIL import Image, ImageDraw, ImageFont
+import torch
+
+from transformers import LayoutLMTokenizer
+from transformers import LayoutLMForTokenClassification
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+args = {'local_rank': -1,
+        'overwrite_cache': True,
+        'data_dir': '/content/drive/MyDrive/LayoutLM/working/dataset',
+        'model_name_or_path':'microsoft/layoutlm-base-uncased',
+        'max_seq_length': 512,
+        'model_type': 'layoutlm',}
+
+# class to turn the keys of a dict into attributes (thanks Stackoverflow)
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+args = AttrDict(args)
+
+tokenizer = LayoutLMTokenizer.from_pretrained("microsoft/layoutlm-base-uncased")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def model_load(PATH, num_labels):
+
+
+    model = LayoutLMForTokenClassification.from_pretrained("microsoft/layoutlm-base-uncased", num_labels=num_labels)
+    model.load_state_dict(torch.load(PATH, map_location=device))
+    model.to(device)
+    model.eval()
+    return model
+
+
+def preprocess(image_path):
+
+    image = Image.open(image_path)
+    image = image.convert("RGB")
+
+    width, height = image.size
+    w_scale = 1000 / width
+    h_scale = 1000 / height
+    ocr_df = pytesseract.image_to_data(image, output_type='data.frame')
+    ocr_df = ocr_df.dropna().assign(left_scaled=ocr_df.left * w_scale,
+                    width_scaled=ocr_df.width * w_scale,
+                    top_scaled=ocr_df.top * h_scale,
+                    height_scaled=ocr_df.height * h_scale,
+                    right_scaled=lambda x: x.left_scaled + x.width_scaled,
+                    bottom_scaled=lambda x: x.top_scaled + x.height_scaled)
+    float_cols = ocr_df.select_dtypes('float').columns
+    ocr_df[float_cols] = ocr_df[float_cols].round(0).astype(int)
+    ocr_df = ocr_df.replace(r'^\s*$', np.nan, regex=True)
+    ocr_df = ocr_df.dropna().reset_index(drop=True)
+
+    words = list(ocr_df.text)
+    coordinates = ocr_df[['left', 'top', 'width', 'height']]
+    actual_boxes = []
+    for idx, row in coordinates.iterrows():
+      x, y, w, h = tuple(row) # the row comes in (left, top, width, height) format
+      actual_box = [x, y, x+w, y+h] # we turn it into (left, top, left+widght, top+height) to get the actual box
+      actual_boxes.append(actual_box)
+    boxes = []
+    for box in actual_boxes:
+        boxes.append(normalize_box(box, width, height))
+    return image, words, boxes, actual_boxes
+
+def normalize_box(box, width, height):
+    return [
+        int(1000 * (box[0] / width)),
+        int(1000 * (box[1] / height)),
+        int(1000 * (box[2] / width)),
+        int(1000 * (box[3] / height)),
+    ]
+
+
+def convert_example_to_features(image, words, boxes, actual_boxes, tokenizer, args, cls_token_box=[0, 0, 0, 0],
+                                sep_token_box=[1000, 1000, 1000, 1000],
+                                pad_token_box=[0, 0, 0, 0]):
+    width, height = image.size
+
+
+    tokens = []
+    token_boxes = []
+    actual_bboxes = []  # we use an extra b because actual_boxes is already used
+    token_actual_boxes = []
+    for word, box, actual_bbox in zip(words, boxes, actual_boxes):
+        word_tokens = tokenizer.tokenize(word)
+        tokens.extend(word_tokens)
+        token_boxes.extend([box] * len(word_tokens))
+        actual_bboxes.extend([actual_bbox] * len(word_tokens))
+        token_actual_boxes.extend([actual_bbox] * len(word_tokens))
+    # Truncation: account for [CLS] and [SEP] with "- 2".
+    special_tokens_count = 2
+    if len(tokens) > args.max_seq_length - special_tokens_count:
+        tokens = tokens[: (args.max_seq_length - special_tokens_count)]
+        token_boxes = token_boxes[: (args.max_seq_length - special_tokens_count)]
+        actual_bboxes = actual_bboxes[: (args.max_seq_length - special_tokens_count)]
+        token_actual_boxes = token_actual_boxes[: (args.max_seq_length - special_tokens_count)]
+    # add [SEP] token, with corresponding token boxes and actual boxes
+    tokens += [tokenizer.sep_token]
+    token_boxes += [sep_token_box]
+    actual_bboxes += [[0, 0, width, height]]
+    token_actual_boxes += [[0, 0, width, height]]
+
+    segment_ids = [0] * len(tokens)
+    # next: [CLS] token
+    tokens = [tokenizer.cls_token] + tokens
+    token_boxes = [cls_token_box] + token_boxes
+    actual_bboxes = [[0, 0, width, height]] + actual_bboxes
+    token_actual_boxes = [[0, 0, width, height]] + token_actual_boxes
+    segment_ids = [1] + segment_ids
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+    # Zero-pad up to the sequence length.
+    padding_length = args.max_seq_length - len(input_ids)
+    input_ids += [tokenizer.pad_token_id] * padding_length
+    input_mask += [0] * padding_length
+    segment_ids += [tokenizer.pad_token_id] * padding_length
+    token_boxes += [pad_token_box] * padding_length
+    token_actual_boxes += [pad_token_box] * padding_length
+    assert len(input_ids) == args.max_seq_length
+    assert len(input_mask) == args.max_seq_length
+    assert len(segment_ids) == args.max_seq_length
+    # assert len(label_ids) == args.max_seq_length
+    assert len(token_boxes) == args.max_seq_length
+    assert len(token_actual_boxes) == args.max_seq_length
+
+    return input_ids, input_mask, segment_ids, token_boxes, token_actual_boxes
+
+
+def convert_to_features(image, words, boxes, actual_boxes, model):
+    input_ids, input_mask, segment_ids, token_boxes, token_actual_boxes = convert_example_to_features(image=image,
+                                                                                                      words=words,
+                                                                                                      boxes=boxes,
+                                                                                                      actual_boxes=actual_boxes,
+                                                                                                      tokenizer=tokenizer,args=args)
+    
+    input_ids = torch.tensor(input_ids, device=device).unsqueeze(0)
+    attention_mask = torch.tensor(input_mask, device=device).unsqueeze(0)
+    token_type_ids = torch.tensor(segment_ids, device=device).unsqueeze(0)
+    bbox = torch.tensor(token_boxes, device=device).unsqueeze(0)
+    #model=model_load(model_path,num_labels)
+    outputs = model(input_ids=input_ids, bbox=bbox, attention_mask=attention_mask, token_type_ids=token_type_ids, return_dict=True)
+    token_predictions = outputs.logits.argmax(-1).squeeze().tolist()  # the predictions are at the token level
+    #print(token_predictions)
+
+    word_level_predictions = []  # let's turn them into word level predictions
+    final_boxes = []
+    dic = {}
+    t = tuple([0,0])
+    
+    #words[0] = "hello"
+    for id, token_pred, box in zip(input_ids.squeeze().tolist(), token_predictions, token_actual_boxes):
+          
+        if (tokenizer.decode([id]).startswith("##")) or (id in [tokenizer.cls_token_id,tokenizer.sep_token_id,tokenizer.pad_token_id]):
+            
+            if (tokenizer.decode([id]).startswith("##")) :
+                s = tokenizer.decode([id])
+                dic[t]+=s[2:]
+            
+            #print(words[len(words)-1])
+            #skip prediction + bounding box
+            #print(tokenizer.decode([id]))
+            continue
+        else:
+            t = tuple(box)
+            if(tuple(box) in dic.keys()):
+                dic[tuple(box)]+=tokenizer.decode([id])
+            else :
+                dic[tuple(box)] = tokenizer.decode([id])
+            #print(tokenizer.decode([id])+ label_map[token_pred])
+            #print(box)
+            word_level_predictions.append(token_pred)
+            final_boxes.append(box)
+            
+    #print("\n\n+llallal")
+    list = [290, 170, 391, 188]
+    #print("str")
+    indices = [i for i, x in enumerate(token_actual_boxes) if x == list]
+    #print(indices)
+            
+    return word_level_predictions, final_boxes,dic
+
+
+
+image_path='r1.jpg'
+image, words, boxes, actual_boxes = preprocess(image_path)
+
+
+
+
+from torch.nn import CrossEntropyLoss
+
+def get_labels(path):
+    with open(path, "r") as f:
+        labels = f.read().splitlines()
+    if "O" not in labels:
+        labels = ["O"] + labels
+    return labels
+
+labels = get_labels("dataset/labels.txt")
+num_labels = len(labels)
+label_map = {i: label for i, label in enumerate(labels)}
+# Use cross entropy ignore index as padding label id so that only real label ids contribute to the loss later
+pad_token_label_id = CrossEntropyLoss().ignore_index
+
+
+model_path='modelFinal.pt'
+model=model_load(model_path,num_labels)
+
+
+
+word_level_predictions, final_boxes,dic = convert_to_features(image, words, boxes, actual_boxes, model)
+
+
+draw = ImageDraw.Draw(image)
+font = ImageFont.load_default()
+def iob_to_label(label):
+    if label != 'O':
+        return label[2:]
+    else:
+        return ""
+label2color = {'total':'green','date':'green','others':'blue','address':'blue','company':'black', '':'blue'}
+#print(word_level_predictions[0:6])
+newl= {}
+cnt = 0
+for prediction, box in zip(word_level_predictions, final_boxes):
+    predicted_label = iob_to_label(label_map[prediction]).lower()
+    if tuple(box) in newl.keys():
+         if newl[tuple(box)] == "" :
+                newl[tuple(box)] = predicted_label
+    else :
+        newl[tuple(box)] = predicted_label
+        
+        #print(newl[tuple(box)])
+    
+    draw.rectangle(box, outline=label2color[predicted_label])    
+    draw.text((box[0] + 10, box[1] - 10), text=predicted_label, fill=label2color[predicted_label], font=font)
+    
+# for key in newl.keys():
+#      print(key,newl[key])
+final_dic = {"address":"", "date":"", "total":"", "company":""}
+for key in newl.keys():
+    if(newl[key] == "address" or newl[key] == "company" ):
+        final_dic[newl[key]]+=dic[key]
+        final_dic[newl[key]]+=" "
+    
+    else:
+        final_dic[newl[key]]=dic[key]
+    #print(key, dic[key])
+for key in final_dic.keys() :
+    print(key, final_dic[key])
+image.save("im.jpg")
+
+
+
+
+
--- flask/py-files/smsProcess.py
+++ flask/py-files/smsProcess.py
@@ -1,3 +1,4 @@
+from gensim.parsing.preprocessing import remove_stopwords
 import spacy
 from spacy import displacy
 
@@ -67,22 +68,24 @@ def getFrontSubString(regex, str):
 
 
 def preProcess(str):
-
+    #str = str.replace("-", " ")
     res = re.sub("(\(.*\))", "", str)
+    res = res.replace("-", " ")
     res = res.replace("(\n)*", " ")
     res = res.replace("  ", " ")
+
     return res
 
 
 def getInfo(sms):
     NER = spacy.load("en_core_web_sm")
     str = sms
-    textTemp = NER(str)
+
     # for word in text3.ents:
     #     print(word.text,word.label_)
 
     nlp = spacy.load('en_core_web_sm')
-    displacy.render(textTemp, jupyter=True)
+
     # for token in text2:
     #      #print (token.text, token.tag_ , token.head, token.dep_)
 
@@ -93,6 +96,11 @@ def getInfo(sms):
             "Amount": "undefined", "Type": "undefined"}
     dict["Date"] = find_date(str)
     # print(str+"\n\n")
+    str.replace("-", " ")
+    str = preProcess(str)
+    print(str)
+    textTemp = NER(str)
+    displacy.render(textTemp, jupyter=True)
 
     if 'c/b' in str.lower():
         temp = getEndSubString("c/b", str)
@@ -107,14 +115,14 @@ def getInfo(sms):
             temp = getEndSubString("payment", str)
             # print(temp)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
         if 'bill' in token.text.lower():
 
             temp = getEndSubString("bill", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
         if 'balance' in token.text.lower():
@@ -122,13 +130,13 @@ def getInfo(sms):
             temp = getEndSubString("balance", str)
             # print(str)
             dict["Balance"] = find_money(temp)
-            #str = str.replace(dict["Balance"], "")
+            # str = str.replace(dict["Balance"], "")
 
         if 'purchase' in token.text.lower():
 
             temp = getEndSubString("purchase", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
         if 'purchased' in token.text.lower():
@@ -136,20 +144,20 @@ def getInfo(sms):
 
             temp = getEndSubString("purchased", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
         if 'paid' in token.text.lower():
 
             temp = getEndSubString("paid", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
         if 'received' in token.text.lower():
             temp = getEndSubString("received", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
 
             for child in token.children:
                 if child.pos_ == 'PRON' and child.text.lower() == 'you':
@@ -161,7 +169,7 @@ def getInfo(sms):
         if 'got' in token.text.lower():
             temp = getEndSubString("got", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
 
             for child in token.children:
                 if child.pos_ == 'PRON' and child.text.lower() == 'you':
@@ -174,20 +182,20 @@ def getInfo(sms):
             flag = 0
             # print("children")
             for child in token.children:
-                #print(child.dep_+" "+child.text)
+                # print(child.dep_+" "+child.text)
                 if child.pos_ == 'ADP' and (child.text.lower() == 'by' or child.text.lower() == 'with'):
                     flag = 1
                     dict["Type"] = "Credit"
                     temp = getEndSubString("credited", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
 
                 if child.pos_ == 'ADP' and child.text.lower() == 'to':
                     flag = 1
                     dict["Type"] = "Credit"
                     temp = getFrontSubString("credited", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
             if flag == 0:
                 temp = getEndSubString("credited", str)
                 dict["Amount"] = find_money(temp)
@@ -195,37 +203,37 @@ def getInfo(sms):
         if 'cash' in token.text.lower():
 
             for child in token.children:
-                #print(child.dep_+" "+child.text)
+                # print(child.dep_+" "+child.text)
                 if child.pos_ == 'ADP' and child.text.lower() == 'in':
                     dict["Type"] = "Income"
                     temp = getEndSubString("cash", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
 
                 if child.pos_ == 'ADP' and child.text.lower() == 'out':
                     dict["Type"] = "Expense"
                     temp = getFrontSubString("cash", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
 
         if 'debited' in token.text.lower():
             flag = 0
             # print("children")
             for child in token.children:
-                #print(child.dep_+" "+child.text)
+                # print(child.dep_+" "+child.text)
                 if child.pos_ == 'ADP' and (child.text.lower() == 'by' or child.text.lower() == 'with'):
                     flag = 1
                     dict["Type"] = "Debit"
                     temp = getEndSubString("debited", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
 
                 if child.pos_ == 'ADP' and child.text.lower() == 'to':
                     flag = 1
                     dict["Type"] = "Debit"
                     temp = getFrontSubString("debited", str)
                     dict["Amount"] = find_money(temp)
-                    #str = str.replace(dict["Amount"], "")
+                    # str = str.replace(dict["Amount"], "")
 
             if flag == 0:
                 temp = getEndSubString("debited", str)
@@ -235,42 +243,42 @@ def getInfo(sms):
 
             temp = getFrontSubString("transferred", str)
             dict["Amount"] = find_money(temp)
-            #str = str.replace(dict["Amount"], "")
+            # str = str.replace(dict["Amount"], "")
             dict["Type"] = "Expense"
 
     return dict
 
 
 sms = []
-sms.insert(len(sms), "Cash In Tk 15,300.00 from 01851557738 successful. Fee Tk 0.00. Balance Tk 15,323.64. TrxID 9EB806B056 at 11/05/2022 13:47. Download App: https://bKa.sh/8app")
-sms.insert(len(sms), "You have received a deposit of BDT 600")
-sms.insert(len(sms), "Bill Payment to DPDC is successful.Amount: BDT 2322.00. Biller Account: 19254909. Fee:0.00 TxnId: 711O3YRZ 11/01/2022 13:36")
-sms.insert(len(sms), "Dear Card Member,your monthly bill against card no 452989*2604 is BDT 560.00 dr. for Sep 21. Min due: $500.00. Last Pmt date Oct-4-2021. Call: 16735")
-sms.insert(len(sms), "We have received your payment of BDT 500 ")
-sms.insert(len(sms), "You have received a deposit of BDT 600")
-sms.insert(len(sms), "Payment of BDT 13.20 is made at Infinity Malls ")
-sms.insert(len(sms), "Dear Card Member, BDT 636.00 has been credited to your MGBL Credit Card 452989*2604 on 27.07.21. Thank you ")
-sms.insert(len(sms), "Dear Sir, your A/C ***7591 debited (Nexus Debit Card Renewal Fee) by Tk460.00 on 08-03-2022 10:54:28 PM C/B Tk19,358.47. NexusPay")
-sms.insert(len(sms), "Cash In Tk 15,300.00 from 01851557738 successful. Fee Tk 0.00. Balance Tk 15,323.64. TrxID 9EB806B056 at 11/05/2022 13:47. Download App: https://bKa.sh/8app")
-sms.insert(len(sms), "Tk30.00 debited to recharge mobile 01515272739, Your A/C Balance:Tk24.00 TxnId: 2925691685 Date:27-APR-22 07:06:28 pm. Pls download https://bit.ly/nexuspay")
-sms.insert(len(sms), "Your Janata Bank account A/C ***7591 has been debited BDT 10,000 at 11/05/2022 13:47. Current balance is BDT 15,000. Thanks")
-sms.insert(len(sms), "Congratulations! You have received Cashback Tk 10. Balance Tk 100. TrxID ***123 at 11/05/2022 13:47")
-sms.insert(
-    len(sms), "Payment Tk 500 to 01456789012 is successful. Balance Tk 10,000.")
+# sms.insert(len(sms), "Cash In Tk 15,300.00 from 01851557738 successful. Fee Tk 0.00. Balance Tk 15,323.64. TrxID 9EB806B056 at 11/05/2022 13:47. Download App: https://bKa.sh/8app")
+# sms.insert(len(sms), "You have received a deposit of BDT 600")
+# sms.insert(len(sms), "Bill Payment to DPDC is successful.Amount: BDT 2322.00. Biller Account: 19254909. Fee:0.00 TxnId: 711O3YRZ 11/01/2022 13:36")
+# sms.insert(len(sms), "Dear Card Member,your monthly bill against card no 452989*2604 is BDT 560.00 dr. for Sep 21. Min due: $500.00. Last Pmt date Oct-4-2021. Call: 16735")
+# sms.insert(len(sms), "We have received your payment of BDT 500 ")
+# sms.insert(len(sms), "You have received a deposit of BDT 600")
+# sms.insert(len(sms), "Payment of BDT 13.20 is made at Infinity Malls ")
+# sms.insert(len(sms), "Dear Card Member, BDT 636.00 has been credited to your MGBL Credit Card 452989*2604 on 27.07.21. Thank you ")
+# sms.insert(len(sms), "Dear Sir, your A/C ***7591 debited (Nexus Debit Card Renewal Fee) by Tk460.00 on 08-03-2022 10:54:28 PM C/B Tk19,358.47. NexusPay")
+# sms.insert(len(sms), "Cash In Tk 15,300.00 from 01851557738 successful. Fee Tk 0.00. Balance Tk 15,323.64. TrxID 9EB806B056 at 11/05/2022 13:47. Download App: https://bKa.sh/8app")
+# sms.insert(len(sms), "Tk30.00 debited to recharge mobile 01515272739, Your A/C Balance:Tk24.00 TxnId: 2925691685 Date:27-APR-22 07:06:28 pm. Pls download https://bit.ly/nexuspay")
+# sms.insert(len(sms), "Your Janata Bank account A/C ***7591 has been debited BDT 10,000 at 11/05/2022 13:47. Current balance is BDT 15,000. Thanks")
+# sms.insert(len(sms), "Congratulations! You have received Cashback Tk 10. Balance Tk 100. TrxID ***123 at 11/05/2022 13:47")
+# sms.insert(
+#     len(sms), "Payment Tk 500 to 01456789012 is successful. Balance Tk 10,000.")
 
-sms.insert(
-    len(sms), "Payment Tk 500 to 01456789012 is successful. Balance Tk 10,000.")
+# sms.insert(
+#     len(sms), "Payment Tk 500 to 01456789012 is successful. Balance Tk 10,000.")
 
-sms.insert(len(sms), '''Your payment for iPayBRTA Tran-No:2103040849330/BDT 23,293.00 is successful. Please print e-Money Receipt from www.ipaybrta.cnsbd.com 
- Thank you.''')
+# sms.insert(len(sms), '''Your payment for iPayBRTA Tran-No:2103040849330/BDT 23,293.00 is successful. Please print e-Money Receipt from www.ipaybrta.cnsbd.com
+#  Thank you.''')
 
-sms.insert(len(sms), "Tk3,000.00 transferred to A/C:017916155391 Fee:Tk.00, Your A/C Balance: Tk1,054.00 TxnId:2890470643 Date:11-APR-22 03:26:20 pm. Download https://bit.ly/nexuspay")
+# sms.insert(len(sms), "Tk3,000.00 transferred to A/C:017916155391 Fee:Tk.00, Your A/C Balance: Tk1,054.00 TxnId:2890470643 Date:11-APR-22 03:26:20 pm. Download https://bit.ly/nexuspay")
 sms.insert(len(sms), "Cash-In from A/C: 019025556564 Tk4,030.00 Fee: Tk.00, Your A/C Balance: Tk4,074.00.TxnId:2868070895 Date:01-APR-22 05:24:25 pm. Download https://bit.ly/nexuspay")
-sms.insert(len(sms), "Dear Card Member, BDT 50000.00 has been credited to your MGBL Credit Card 452989*2604 on 23.05.21. Thank you")
+# sms.insert(len(sms), "Dear Card Member, BDT 50000.00 has been credited to your MGBL Credit Card 452989*2604 on 23.05.21. Thank you")
 for res in sms:
     print("\n\n"+res+"\n\n")
-    res = preProcess(res)
+   # res = preProcess(res)
     print(getInfo(res))
 
-# res = preProcess(sms[len(sms)-1])
-# print(getInfo(res))
+
+# print(getInfo(sms[0]))
